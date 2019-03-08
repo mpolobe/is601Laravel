2f6476a41c379bbc68cec2a8088cd465bc86c5aa
Ben Mpolokoso <ben.mpolokoso@gmail.com>
2019-03-08 20:00:15 +0200

    Created Home Page and About Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    I am the Home Page
+@stop
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
